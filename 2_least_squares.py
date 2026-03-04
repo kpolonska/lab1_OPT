096e3f2640849761e5f8cfc36416e7b9f5c3ad42
Kristina Polonska <kristina.ppolonska@gmail.com>
2026-03-04 18:23:19 +0200

    add visualisation

--- 2_least_squares.py
+++ 2_least_squares.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 def calculate_objective(x, A, b):
     m = A.shape[0]
@@ -17,6 +18,7 @@ def gradient_descent(x0, A, b, lamda, max_iter, stop_cr):
     x = x0.copy()
 
     start_time = time.time()
+    objective_values = []
 
     for k in range(max_iter):
         g = compute_gradient(x, A, b)
@@ -24,15 +26,16 @@ def gradient_descent(x0, A, b, lamda, max_iter, stop_cr):
         x1_new = x - lamda * g
 
         f = calculate_objective(x1_new, A, b)
+        objective_values.append(f)
         if np.linalg.norm(g) < stop_cr:
             print(f'Found the min of f: {f}. The point x=[{x1_new}]')
             print(f'Time taken: {time.time() - start_time}. Number of iterations: {k+1}.')
-            return f
+            return objective_values
 
         x = x1_new
 
     print('Exceeded number of iterations. Exited function.')
-    return 0
+    return objective_values
 
 A = np.array([[2, 3, 4], [3, 5, 1], [5, 6, 7]], dtype=float)
 b = np.array([1, 8, 9], dtype=float)
@@ -45,8 +48,22 @@ two = np.linalg.norm(A.T @ b)
 
 L = (1/3) * (one * 20 + two)
 lamdas = [0.1, 1/beta, 1/L]
+labels = ['λ = 0.1', 'λ = 1/β', 'λ = 1/L']
 
+results = []
 for i in lamdas:
-    gradient_des = gradient_descent(x0, A, b, i, 10000, 0.001)
+    gradient_des = gradient_descent(x0, A, b, i, 50, 0.001)
+    results.append(gradient_des)
+
+
+for i, (result, label) in enumerate(zip(results, labels)):
+    plt.plot(result, label=label)
+
+plt.xlabel('Iteration')
+plt.ylabel('Objective Function Value')
+plt.legend()
+plt.grid(True)
+plt.yscale('log')
+plt.show()
 
 
--- 3_fixed _point.py
+++ 3_fixed _point.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 def calculate_objective(x, f):
     if f == 1:
@@ -29,12 +30,14 @@ def gradient_descent(x, lamda, max_iter, stop_cr, func_number):
 
     start_time = time.time()
     prev_step = calculate_objective(x, func_number)
+    objective_values = []
 
     for k in range(max_iter):
 
         gradient = compute_gradient(x1, func_number)
         x1_new = x1 - lamda * gradient
         f = calculate_objective(x1_new, func_number)
+        objective_values.append(f)
 
         if k % 10 == 0:
             print(f'On iteration {k}: value f = {f}, x = {x1_new}')
@@ -42,19 +45,34 @@ def gradient_descent(x, lamda, max_iter, stop_cr, func_number):
         if abs(prev_step - f) < stop_cr:
             print(f'Found the min of f: {f}. The point [{x1_new}]')
             print(f'Time taken: {time.time() - start_time}. Number of iterations: {k+1}.')
-            return f
+            return objective_values
 
         x1 = x1_new
         prev_step = f
 
     print('Exceeded number of iterations. Exited function.')
-    return 0
+    return objective_values
 
 
 x = 2 # because the region is [0, 2] and we take the max
 L1 = (x - np.log(1 + x)) * (1 - (1 / (1 + x))) # online calc
 L2 = (x - np.log(2 + x)) * (1 - (1 / (2 + x)))  # online calc
 
+results = []
+labels = ['L1', 'L2']
+
 result_g1 = gradient_descent(0.1, 1/L1, 100, 0.00000001, 1)
 result_g2 = gradient_descent(0.1, 1/L2, 100, 0.00000001, 2)
 
+results.append(result_g1)
+results.append(result_g2)
+
+for i, (result, label) in enumerate(zip(results, labels)):
+    plt.plot(result, label=label)
+
+plt.xlabel('Iteration')
+plt.ylabel('Function Value')
+plt.legend()
+plt.grid(True)
+plt.yscale('log')
+plt.show()
